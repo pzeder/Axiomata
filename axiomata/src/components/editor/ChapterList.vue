e8d8041100d3f66490a8e66faa37eceb61d4b57f
pzeder <philippe.zeder@gmail.com>
2023-11-27 00:08:50 +0100

    input bar test

--- axiomata/src/components/editor/ChapterList.vue
+++ axiomata/src/components/editor/ChapterList.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="chapter-container" v-for="(chapter, index) in chapters" :key="index"> {{ chapter.title }} </div>
+  <div class="chapter-container" v-for="(chapter, index) in chapters" :key="index">
+    <div> {{ chapter.title }}</div>
+    <div> Neues Level hinzufügen </div>
+    <input type="text" id="textInput" placeholder="Enter text here">
+  </div>
   <div class="new-chapter-button" @click="emit('addNewChapter')"> Neues Kapitel hinzufügen </div>
 </template>
 
@@ -21,7 +25,7 @@ const emit = defineEmits(['addNewChapter']);
   width: 100vw;
   height: 10vh;
   border: 2px solid black;
-  border-radius: 5vw;
+  border-radius: 1vw;
   margin-bottom: 10px;
   display: flex;
   align-items: center;
@@ -42,4 +46,16 @@ const emit = defineEmits(['addNewChapter']);
   font-size: 20pt;
   user-select: none;
 }
-</style>
+
+#textInput {
+  width: 300px;
+  /* Width of the text bar */
+  padding: 10px;
+  /* Padding inside the text bar */
+  margin: 10px;
+  /* Margin around the text bar */
+  border: 1px solid #ccc;
+  /* Border of the text bar */
+  border-radius: 4px;
+  /* Rounded corners for the text bar */
+}</style>
