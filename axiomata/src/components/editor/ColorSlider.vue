71bddffba2714bddebdb8c9cf2bcdbe283c79075
pzeder <philippe.zeder@gmail.com>
2023-12-01 17:10:12 +0100

    backroundColor sliders work

---
+++ axiomata/src/components/editor/ColorSlider.vue
@@ -0,0 +1,66 @@
+<template>
+  <div ref="rootElement" class="slider-container">
+    <input type="range" min="0" max="255" v-model="sliderValue" class="slider" id="myRange"
+      @mousemove="emit('changeValue', sliderValue)">
+    <div> {{ sliderValue }} </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { Ref, ref, defineEmits, defineProps, onMounted } from 'vue';
+
+interface Props {
+  color: string;
+}
+
+const rootElement: Ref<HTMLElement | null> = ref(null);
+const props = defineProps<Props>();
+const emit = defineEmits(['changeValue']);
+const sliderValue = ref(255);
+
+onMounted(() => {
+  if (rootElement.value) {
+    rootElement.value.style.setProperty('--slider-thumb-color', props.color);
+  }
+});
+</script>
+
+<style scoped>
+.slider-container {
+  width: 50%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 25px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  background: var(--slider-thumb-color);
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  background: var(--slider-thumb-color);
+}
+</style>
+
--- axiomata/src/components/editor/SymbolEditor.vue
+++ axiomata/src/components/editor/SymbolEditor.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="backdrop" />
   <div class="edit-symbol-container">
-    <div class="edit-symbol" :style="{ background: backgroundColor, color: textColor }">
-      {{ text }}
+    <div class="flex-container">
+      <div class="slider-collection">
+        <ColorSlider color="red" @changeValue="setBackgroundRed" />
+        <ColorSlider color='green' @changeValue="setBackgroundGreen" />
+        <ColorSlider color='blue' @changeValue="setBackgroundBlue" />
+      </div>
+      <div class="edit-symbol" :style="{ background: backgroundColor, color: textColor }">
+        {{ text }}
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue';
+import { ComputedRef, Ref, computed, ref } from 'vue';
+import ColorSlider from '@/components/editor/ColorSlider.vue';
 
-const backgroundColor: Ref<string> = ref('red');
+const backgroundColor: ComputedRef<string> = computed(() =>
+  'rgb(' + backGroundRed.value + ',' + backGroundGreen.value + ',' + backGroundBlue.value + ')');
 const text: Ref<string> = ref('text');
 const textColor: Ref<string> = ref('blue');
+
+const backGroundRed: Ref<number> = ref(255);
+const backGroundGreen: Ref<number> = ref(255);
+const backGroundBlue: Ref<number> = ref(255);
+
+function setBackgroundRed(value: number): void {
+  backGroundRed.value = value;
+}
+
+function setBackgroundGreen(value: number): void {
+  backGroundGreen.value = value;
+}
+
+function setBackgroundBlue(value: number): void {
+  backGroundBlue.value = value;
+}
 </script>
 
 <style>
+.backdrop {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background: black;
+  opacity: 20%;
+}
+
 .edit-symbol-container {
   position: fixed;
   left: 0;
@@ -26,14 +61,17 @@ const textColor: Ref<string> = ref('blue');
   place-items: center;
 }
 
-.backdrop {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background: black;
-  opacity: 20%;
+.flex-container {
+  display: flex;
+  justify-content: center;
+}
+
+.slider-collection {
+  width: 30vw;
+  height: 20vw;
+  background-color: white;
+  display: grid;
+  place-items: center;
 }
 
 .edit-symbol {
