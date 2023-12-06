e01a33ab6f78e68f7fa2ac8b9ae0366a87aae87e
pzeder <philippe.zeder@gmail.com>
2023-12-06 18:11:46 +0100

    added empty button to editLevel

--- axiomata/src/components/editor/chapterEditor/EditLevel.vue
+++ axiomata/src/components/editor/chapterEditor/EditLevel.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="edit-level">
     <div :style="{ display: 'flex', alignItems: 'center' }">
+      <div class="empty-button" v-if="!axiomValid(level.goalAxiom)" @click="emit('editGoalAxiom')">
+        START / ZIEL festlegen
+      </div>
       <AxiomContainer v-if="axiomValid(level.goalAxiom)" :width="20" :height="20" :axiom="level.goalAxiom"
         :symbols="symbols" :variables="[]" :varColors="[]" @click="emit('editGoalAxiom')" />
       <TitleBar :title="level.title" :height=5 @editTitle="emit('editLevelTitle')" />
@@ -35,5 +38,15 @@ const emit = defineEmits(['editLevelTitle', 'deleteLevel', 'editGoalAxiom']);
   font-size: 20pt;
   user-select: none;
   background-color: rgb(255, 157, 0);
+}
+
+.empty-button {
+  display: grid;
+  place-items: center;
+  border: 0.2vw solid black;
+  border-radius: 2vw;
+  font-size: 2vw;
+  padding: 1vw;
+  background: rgb(247, 247, 110);
 }
 </style>
