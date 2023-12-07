017aade46a3ef9fa9f358b4880ed41301e8466f5
pzeder <philippe.zeder@gmail.com>
2023-12-07 18:31:31 +0100

    added submit button to editorScreen

--- axiomata/src/components/editor/EditorScreen.vue
+++ axiomata/src/components/editor/EditorScreen.vue
@@ -2,6 +2,7 @@
   <TitleBar tag="Kurs" :title="course?.title" :height=10 @editTitle="showTextInput = true" />
   <EditChapterList v-if="course" :editID="editID" :course="course" @updateChapters="updateChapters"
     @updateSymbols="updateSymbols" />
+  <div class="submit-button" :style="{ background: courseValid ? 'lightgreen' : 'gray' }"> Kurs hochladen </div>
   <TextInput v-if="showTextInput" title="Titel des Kurses ändern" :placeholder="course?.title"
     @updateText="updateCourseTitle" @click="showTextInput = false" />
 </template>
@@ -9,7 +10,7 @@
 <script setup lang="ts">
 import axios from 'axios';
 import { Ref, ref, defineProps, onMounted, ComputedRef, computed } from 'vue';
-import { CourseData, ChapterData, SymbolData, LevelPointer } from '@/scripts/Interfaces';
+import { CourseData, ChapterData, SymbolData, LevelData } from '@/scripts/Interfaces';
 import EditChapterList from './chapterEditor/EditChapterList.vue';
 import TextInput from './TextInput.vue';
 import TitleBar from './TitleBar.vue';
@@ -22,6 +23,19 @@ const props = defineProps<Props>();
 
 const course: Ref<CourseData | null> = ref(null);
 const showTextInput: Ref<boolean> = ref(false);
+
+const invalidLevel = (level: LevelData): boolean =>
+  level.goalAxiom.upperSequence.length === 0 || level.goalAxiom.lowerSequence.length === 0;
+
+const invalidChapter = (chapter: ChapterData): boolean =>
+  chapter.levels.length === 0 || chapter.levels.some(invalidLevel);
+
+const courseValid: ComputedRef<boolean> = computed(() => {
+  if (!course.value) {
+    return false;
+  }
+  return course.value.chapters.filter(invalidChapter).length === 0;
+});
 
 onMounted(() => {
   fetchEdit();
@@ -75,3 +89,16 @@ function updateSymbols(updatedSymbols: SymbolData[]): void {
   }
 }
 </script>
+
+<style>
+.submit-button {
+  display: grid;
+  place-items: center;
+  padding: 1vw;
+  font-size: 3vw;
+  color: black;
+  background: lightgreen;
+  border: 1vw solid black;
+  border-radius: 2vw;
+}
+</style>
