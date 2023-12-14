cb6b566a9001c1487f5891b696facfb1375fb8b9
pzeder <philippe.zeder@gmail.com>
2023-12-14 13:25:30 +0100

    update HomeScreen layout

--- axiomata/src/components/menus/HomeScreen.vue
+++ axiomata/src/components/menus/HomeScreen.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="start-container">
-    <button @click="emit('openNewCourseMenu')"> Neues Spiel </button>
-    <button @click="emit('openSaveStateMenu')"> Laden </button>
-    <button @click="emit('openEditSelection')"> Editor </button>
+  <div class="screen-container">
+    <div class="menu-button-container">
+      <div class="menu-button" @click="emit('openNewCourseMenu')" :style="{ background: 'rgb(255,255,150)'}"> Neues Spiel </div>
+      <div class="menu-button" @click="emit('openSaveStateMenu')" :style="{ background: 'rgb(255,150,255)'}"> Laden </div>
+      <div class="menu-button" @click="emit('openEditSelection')" :style="{ background: 'rgb(150,255,255)'}"> Editor </div>
+    </div>
   </div>
 </template>
 
@@ -16,15 +18,39 @@ const screenHeight: Ref<number> = ref(window.innerHeight);
 </script>
 
 <style>
-.start-container {
-  display: flex;
-  flex-direction: column;
-  margin-top: 30vh;
+.screen-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  place-items: center;
 }
 
-.start-container button {
-  margin-bottom: 10vw;
-  font-size: 50pt;
-  color: black;
+.menu-button-container {
+  display: grid;
+  place-items: center;
+}
+
+.menu-button {
+  font-size: 4vw;
+  color: rgb(44,44,44);
+  border: 0.5vw solid rgb(44,44,44);
+  border-radius: 1vw;
+  padding: 2vw;
+  width: 30vw;
+  display: grid;
+  place-items: center;
+  user-select: none;
+  margin-bottom: 3vw;
+  opacity: 85%;
+  transform: scale(1);
+  transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+}
+
+.menu-button:hover {
+  opacity: 100%;
+  transform: scale(1.04); 
 }
 </style>
